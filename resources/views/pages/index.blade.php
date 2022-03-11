9c5bae69d61d4c5f0fc835b6de668f0c416b58a1
Kiran Anand <liteninkiran@hotmail.com>
2022-03-11 22:30:16 +0000

    Add basic views/controller

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>{{ config('app.name') }}</title>
+    </head>
+    <body>
+        <h1>Welcome to Laravel</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi, nihil vero ducimus cupiditate quod tenetur facere itaque eius vitae culpa. Corporis tempore nam sint sapiente? Incidunt perspiciatis corrupti maxime velit?</p>
+    </body>
+</html>
---
+++ resources/views/pages/services.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>{{ config('app.name') }}</title>
+    </head>
+    <body>
+        <h1>Services</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis illo ipsa voluptas ipsum? Modi sapiente minima eius perspiciatis nam quae repudiandae sint, id porro alias, neque provident illo adipisci ab.</p>
+    </body>
+</html>
