ec8117040535eb5b6dab1b9a57422eb398d50871
Kiran Anand <liteninkiran@hotmail.com>
2022-03-11 22:33:43 +0000

    Split into template

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>{{ config('app.name') }}</title>
-    </head>
-    <body>
-        <h1>About</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero eum sapiente voluptates voluptatum. Mollitia natus totam magni iure molestiae impedit ex dolore excepturi laboriosam qui nobis, ut, doloribus provident odit!</p>
-    </body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <h1>About</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero eum sapiente voluptates voluptatum. Mollitia natus totam magni iure molestiae impedit ex dolore excepturi laboriosam qui nobis, ut, doloribus provident odit!</p>
+@endsection
--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>{{ config('app.name') }}</title>
-    </head>
-    <body>
-        <h1>Services</h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis illo ipsa voluptas ipsum? Modi sapiente minima eius perspiciatis nam quae repudiandae sint, id porro alias, neque provident illo adipisci ab.</p>
-    </body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <h1>Services</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis illo ipsa voluptas ipsum? Modi sapiente minima eius perspiciatis nam quae repudiandae sint, id porro alias, neque provident illo adipisci ab.</p>
+@endsection
